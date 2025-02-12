239538889eea85444c0e78d298f37734fd5d990b
shahzaib-k <shahzaibkhan.4659@gmail.com>
2025-02-12 22:21:28 +0500

    bug fix

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -63,7 +63,7 @@ const login = async (req, res) => {
 const logout = async (req, res) => {
 
     try {
-      res.clearCookie("token" , {secure: true, sameSite: 'none', httpOnly: true})   
+      res.clearCookie("token" , {secure: true, sameSite: 'Lax', httpOnly: true})   
 
       res.status(200).json({message: "Logged out Successfully"})
     } catch (error) {
