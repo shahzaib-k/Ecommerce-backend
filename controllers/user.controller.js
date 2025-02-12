2c99b038a2f21c310d0d7fdd87d8b03d187dda61
shahzaib-k <shahzaibkhan.4659@gmail.com>
2025-02-12 16:28:14 +0500

    bug fix

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -60,15 +60,15 @@ const login = async (req, res) => {
     }
 }
 
-
 const logout = async (req, res) => {
 
     try {
-      res.clearCookie("token", {secure: true, sameSite: none})   
+      res.clearCookie("token" , {secure: true, sameSite: 'none'})   
 
       res.status(200).send({message: "Logged out Successfully"})
     } catch (error) {
         console.log(error);
+        res.status(500).send("Internal Server Error")
     }
 }
 
